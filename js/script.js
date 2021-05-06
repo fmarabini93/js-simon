2d1c01cd1daf59dc89ed38a4ccc30bce9dab31e8
fmarabini93 <fmarabini@icloud.com>
2021-05-06 16:41:10 +0200

    added-js-comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+// Check if an element is in an array
 function inArray(element, list) {
+    
     for (i = 0; i < list.length; i ++) {
         if (element == list[i]) {
             return true;
@@ -9,30 +12,45 @@ function inArray(element, list) {
     }
     return false;
 }
+//
 
+// Print 5 random numbers
 var numbers = [];
-for (i = 0; i < 5; i ++) {
+for (i = 0; i < 5; i ++) {    
+    
     do {
         var number = getRandomNumber(1, 100);
-    } while (inArray(number, numbers) == true)
+    } while (inArray(number, numbers) == true) // --> Check if a number is repeated
+    
     numbers.push(number);
 }
 alert("These are the numbers you must memorize " + numbers);
+//
 
-setTimeout(insertNumbersAndCheck, 3000);
-function insertNumbersAndCheck(choices) {
+
+
+// User types 5 numbers and program checks if present in previous array
+setTimeout(function() {   
+    
     var choice;
-    var wrongChoices = [];
-    var correctChoices = [];
+    var choices = [];
+    correctNumbers = [];
+
     for (i = 0; i < 5; i ++) {
         do {
             choice = parseInt(prompt("Please type a number"))
-        } while (isNaN(choice) == true || choice < 1 || choice > 100)
-        if (inArray(choice, numbers) == true) {
-            correctChoices.push(choice);
-        } else {
-            wrongChoices.push(choice);
+        } while (isNaN(choice) == true || choice < 1 || choice > 100) // --> Check if entry is a number between 1 and 100
+
+        choices.push(choice);
+    }
+
+    for (i = 0; i < 5; i ++) {
+        if (inArray(choices[i], numbers) == true) {
+            correctNumbers.push(choices[i]);
         }
     }
-    console.log(correctChoices);
-}
+
+    console.log(correctNumbers);
+
+}, 3000)
+//
